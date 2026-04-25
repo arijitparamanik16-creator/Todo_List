7b15ccfe045b5d1c5fd8f685ed31d58f67eab545
Arijit Pramanik <arijitparamanik16@gmail.com>
2026-04-25 10:11:17 +0530

    adding proper responsive animation on footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className=' footer w-full bg-blue-400 overflow-hidden'>
 
-        <div className='font-bold sm:text-xl px-10 py-2 whitespace-nowrap text-white animation_scroll'> Remember your tasks are saved into localStorage , if you delete the tasks it will be also removed from localStorage</div>
+        <div className='font-bold sm:text-xl px-10 py-2 whitespace-nowrap text-white animation_scroll '> Remember your tasks are saved into localStorage , if you delete the tasks it will be also removed from localStorage</div>
       
     </div>
   )
